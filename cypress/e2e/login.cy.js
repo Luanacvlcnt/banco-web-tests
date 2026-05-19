a1100ca67ab467811f12e55eb1e49264674d3474
Luanacvlcnt <lua.cavalcanti19@gmail.com>
2026-05-19 13:09:45 -0300

    Adiciona teste para validar credenciais invalidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,9 +6,24 @@ describe('template spec', () => {
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+
+  describe('template spec', () => {
+    it('Login com dados inválidos deve rtornar mensagem de erro', () => {
+      //Arrange
+      cy.visit('http://localhost:4000/')
+
+      //Act
+      cy.get('#username').click().type('luana.freitas')
+      cy.get('#senha').type('123456')
+      cy.contains('button', 'Entrar').click()
+
+      //Assert
+      cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+    })
   })
 })
